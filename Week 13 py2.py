b1d7e2811513cfafe110925f491b7bc67b6acbda
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-12-03 16:38:17 -0600

    Update Week 13 py2.py

--- Week 13 py2.py
+++ Week 13 py2.py
@@ -1,7 +1,11 @@
-
-def display_cities(cur):
+#Braden Phetsarath
+#12/3
+# Cities.db Display
+import sqlite3
+def main(cur):
     print ("Contents cities.db/Cities table: ")
     cur.execute("SELECT * FROM cities")
     resultls = cur.fetchall()
     for row in resultls:
         print(f'{row[0]:<3}{row[1]:20}{row[2]:,.0f}')
+main()
